3e9dd6c502076db27eae147b429463acad71b22a
Lance H <lsaucyfx@gmail.com>
2024-02-13 07:58:42 -0500

    created User Class

---
+++ User.py
@@ -0,0 +1,40 @@
+import ObjectId
+class User(object):
+
+    Users = []
+
+    def __init__(self, userId: ObjectId, username: str, email: str, password: str):
+
+        self.userId = userId
+        self.username = username
+        self.email = email
+        self.password = password
+
+
+    def register(Users):
+        
+        # generate user iD 1001,1002 etc
+
+        # save to variable newUser
+        newUser = 0
+        # enter username
+
+        # enter user email
+
+        # enter user password
+
+        # add user to Users[]
+
+        print("User " + str(NewUser) + " has been successfully registered..")
+
+    def login():
+
+        # implement authentication methods from API
+        pass
+
+
+
+    def logout():
+
+        # implement authentication methods from API
+        pass
